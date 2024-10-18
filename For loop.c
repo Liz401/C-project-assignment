04f81ec9c8b51cee6f059c3cf3cb3ba7fe0b0cd0
Liz401 <amondielizabeth34@gmail.com>
2024-10-18 11:20:40 +0300

    For loop.c

---
+++ For loop.c
@@ -0,0 +1,20 @@
+//use of the loop for 
+#include<stdio.h>
+int main(){
+int v;
+int start;
+int stop;
+printf("Enter the Start Value:");
+scanf("%d\n",start);
+printf("Enter the Stop Value:");
+scanf("%d\n", stop);
+for(int v=start; v<=stop;v++){
+printf("%d\n",v);
+}
+return 0;
+}
+    
+
+
+
+    
