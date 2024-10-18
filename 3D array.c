b12368cd05c1f4588ac44202eadeffe921a6fad7
Liz401 <amondielizabeth34@gmail.com>
2024-10-18 11:27:28 +0300

    3D array.c

---
+++ 3D array.c
@@ -0,0 +1,19 @@
+//Arrays in 3D
+#include<stdio.h>
+int main(){
+int a,b,c;
+int days[2][2][3]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+for(a=0;a<2;a++);{
+for(b=0;b<2;b++);{
+for(c=0;c<3;c++);{
+    printf("Present on days [%d][%d][%d]=%d\n",a,b,c ,days [a][b][c]);
+    }
+    }
+    }
+    return 0;
+    }
+    
+
+
+
+    
